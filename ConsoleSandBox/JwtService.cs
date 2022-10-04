9283ee881ebb40e89fc06b8e23f9dd9ff03de1c6
Paul Matovich <matovich@hotmail.com>
2022-10-04 17:08:06 -0500

    Update .gitignore

--- ConsoleSandBox/JwtService.cs
+++ ConsoleSandBox/JwtService.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
-
-using System.ServiceModel.Security.Tokens;
 
 namespace ConsoleSandBox
 {
